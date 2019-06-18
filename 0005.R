266e5ad687b673f603de482997f95da031dcc81d
EGrandgi <45683057+EGrandgi@users.noreply.github.com>
2019-06-18 11:25:05 +0200

    Create 0005.R

---
+++ 0005.R
@@ -0,0 +1,20 @@
+### 0005
+is.mdiv <- function(n, until){
+  i <- 0
+  while (i < until){
+    i <- i+1
+    if (n%%i == 0){answer <- TRUE} else {
+      answer <- FALSE
+      i <- until + 1
+    }
+  }
+  answer
+}
+
+smallest_mdiv <- function(until){
+  n <- 1
+  while (is.mdiv(n, until) == FALSE){
+    n <- n+1
+  }
+  n
+}
