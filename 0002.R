6e2f0acb8ed737d6e46e47b24cf2865751de9a9b
EGrandgi <45683057+EGrandgi@users.noreply.github.com>
2019-06-18 11:12:50 +0200

    Create 0002.R

---
+++ 0002.R
@@ -0,0 +1,17 @@
+### 0002
+fibonacci <- numeric(50)
+fibonacci[1] <- 1
+fibonacci[2] <- 2
+
+for (i in 3:50) {
+  fibonacci[i] <- fibonacci[i-1] + fibonacci[i-2]
+}
+
+
+sum <- 0
+
+for (i in 1:length(fibonacci)){
+  if (fibonacci[i]%%2 == 0 & fibonacci[i] < 4000000){
+    sum <- sum + fibonacci[i]
+  }
+}
