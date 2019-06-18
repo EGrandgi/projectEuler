870979dc530a1002283bc7dd8dd6bfe7f842390d
EGrandgi <45683057+EGrandgi@users.noreply.github.com>
2019-06-18 11:13:12 +0200

    Create 0003.R

---
+++ 0003.R
@@ -0,0 +1,28 @@
+### 0003
+# prime numbers
+prime_numbers <- c(2, 3, 5, 7)
+for (n in (2:10000)){
+  if (n%%2 != 0 & n%%3 != 0 & n%%5 != 0 & n%%7 != 0){
+    prime_numbers <- c(prime_numbers, n)
+  }
+}
+
+# prime factors
+prime_factors <- c()
+for (p in prime_numbers){
+  if (600851475143%%p == 0){
+    prime_factors <- c(prime_factors, p)
+  }
+}
+
+# OU
+k <- 600851475143
+prime_factors <- c()
+while (k>1){
+  i <- 2
+  while (k%%i != 0){
+    i <- i+1
+  }
+  k <- k/i
+  prime_factors <- c(prime_factors, i)
+}
