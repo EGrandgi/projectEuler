47bf2ab5168f8279451ed37d5f6fbef7798ba02e
EGrandgi <45683057+EGrandgi@users.noreply.github.com>
2019-06-19 23:07:57 +0200

    Create 0020.R

---
+++ 0020.R
@@ -0,0 +1,13 @@
+options("scipen" = 10000, "digits" = 22)
+
+fac <- function(n){
+  if (n != 1){n*fac(n-1)}
+  else {n}
+}
+
+n <- fac(100)
+
+sum_ <- 0
+for (c in 1:nchar(n)){
+  sum_ <- sum_ + as.numeric(substr(n, c, c))
+}
