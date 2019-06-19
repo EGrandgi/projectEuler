3aaa5f9d6069545beda624de22a5b561bac28f90
EGrandgi <45683057+EGrandgi@users.noreply.github.com>
2019-06-19 10:41:12 +0200

    Update 0012.R

--- 0012.R
+++ 0012.R
@@ -1,46 +1,22 @@
 ### 0012
-triangle_number <- function(until){
-  t <- 0
-  for (i in (1:until)){
-    t <- t + i
-  }
-  t
-}
-
-triangle_numbers_seq <- c()
-for (i in (1:1000)){
-  triangle_numbers_seq <- c(triangle_numbers_seq, triangle_number(i))
-}
-
-is.prime <- function(n) n == 2L || all(n %% 2L:max(2,floor(sqrt(n))) != 0)
-
-prime_numbers <- c()
-for (n in (2:100000000)){
-  if (is.prime(n)){
-    prime_numbers <- c(prime_numbers, n)
-  }
-}
-
-prime_factors <- function(n){
-  prime_factors_list <- c()
-  for (p in (prime_numbers)){
-    while (n%%p == 0){
-      prime_factors_list <- c(prime_factors_list, p)
-      n <- n/p
+count_divisors <- function(n){
+  count <- 0
+  for (i in 1:(floor(sqrt(n))+1)){
+    if (n%%i == 0){
+      count <- count + 2
+    }
+    if (i*i == n){
+      count <- count -1
     }
   }
-  prime_factors_list
+  count
 }
 
-
-count_divisors <- function(n){
-  t <- table(prime_factors(n))
-  prod(t+1)
+until <- 0
+c <- 0
+while (c < 500){
+  until <- until +1
+  n <- (until)*(until +1)/2
+  c <- count_divisors(n)
 }
-
-for (n in (triangle_numbers_seq)){
-  if(count_divisors(n)>500){
-    print(n)
-    stop()
-  }
-}
+print(n)
