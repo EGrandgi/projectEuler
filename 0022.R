f66b94e6625f43e5ab9f2c382ceda5555f6f8a21
EGrandgi <45683057+EGrandgi@users.noreply.github.com>
2019-06-20 10:37:31 +0200

    Create 0022.R

---
+++ 0022.R
@@ -0,0 +1,23 @@
+### 0022
+alpha_values <- as.data.frame(setNames(list(LETTERS, c(1:26)),c("alpha","num")))
+
+file <- read.delim("/home/BVA/emma2/p022_names.txt", header = FALSE, sep = ",")
+colnames(file) <- c(1:length(file))
+file[which(is.na(file))] <- "NA"
+file <- t(file)
+file <- sort(file)
+
+alpha_score <- function(name){
+  score <- 0
+  for (c in (1:nchar(name))){
+    score <- score + alpha_values$num[alpha_values$alpha == substr(name, c, c)]
+  }
+  score
+}
+
+sum_ <- 0
+for (i in (1:length(file))){
+  name <- file[i]
+  sum_ <- sum_ + alpha_score(name)*i
+}
+
