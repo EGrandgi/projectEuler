73aa19cca746838b3b17d62eba56f8fcc9807b9e
EGrandgi <45683057+EGrandgi@users.noreply.github.com>
2019-06-20 17:10:38 +0200

    Update 0023.R

--- 0023.R
+++ 0023.R
@@ -1,4 +1,6 @@
 ### 0023
+library(pbmcapply)
+
 d <- function(n){
   sum_ <- 0
   if (n == 1){sum_ <- 1}
@@ -8,17 +10,32 @@ d <- function(n){
         sum_ <- sum_ + k}}}
   sum_}
 
-abundant <- c()
-for (n in (1:28111)){
-  if(d(n) > n){abundant <- c(abundant, n)}
-}
+abundant <- list()
+abundant <- pbmclapply((1:28111), function(n){
+  if(d(n) > n){abundant[length(abundant)+1] <- n}
+}, mc.cores = 10)
+abundant <- unlist(abundant)
 
-can <- c()
-for (i in (1:length(abundant))){
-  for (j in (i+1):length(abundant)){
-    if (i+j < 28123){
-    can <- c(can, i+j)}
-  }}
+# abundant <- c()
+# for (n in (1:28111)){
+#   if(d(n) > n){abundant <- c(abundant, n)}
+# }
+
+can <- list()
+can <- pbmclapply((1:length(abundant)), function(i){
+  pbmclapply((i:length(abundant)), function(j){
+    if (i+j < 28123){can[length(can)+1] <- i+j}
+  }, mc.cores = 10)
+}, mc.cores = 10)
+
+
+# can <- c()
+
+#for (i in (1:length(abundant))){
+#  for (j in (i+1):length(abundant)){
+#    if (i+j < 28123){
+#      can <- c(can, i+j)}
+# }}
 
 can <- sort(unique(can))
 
