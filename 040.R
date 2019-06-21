a048d0bea86c367ce283e56410336608d8a700d0
EGrandgi <45683057+EGrandgi@users.noreply.github.com>
2019-06-21 18:46:06 +0200

    Create 040.R

---
+++ 040.R
@@ -0,0 +1,9 @@
+library(gmp)
+N <- as.bigz(gsub(", ", "", toString(c(1:1000000))))
+
+vect_n <- c(1, 10, 100, 1000, 10000, 100000, 1000000)
+p <- 1
+for (n in vect_n){
+  p <- p*as.numeric(substr(N, n, n))
+}
+p
