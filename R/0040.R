09210b7da2e575361939ec6d66b8a8d4d1829b51
EGrandgi <45683057+EGrandgi@users.noreply.github.com>
2019-06-22 00:30:58 +0200

    Update 0040.R

--- R/0040.R
+++ R/0040.R
@@ -1,18 +1,11 @@
 ### 0040
-# N <- ""
-# M <- 1
-# while (nchar(N) < 1000000){
-#   N <- paste(N, M, sep = "")
-#   M <- M+1
-# }
-
 library(gmp)
-N <- as.bigz(as.numeric(gsub(", ", "", toString(N))))
+N <- as.bigz(gsub(", ", "", toString(c(1:1000000))))
 
 vect_n <- c(1, 10, 100, 1000, 10000, 100000, 1000000)
-sum_ <- 0
+p <- 1
 for (n in vect_n){
-  sum_ <- sum_ + as.numeric(substr(N, n, n))
+  p <- p*as.numeric(substr(N, n, n))
 }
-sum_
+p
 
