da68ff640307ab44c59e58499156c0a58e0f95c2
EGrandgi <egrandgi@gmail.com>
2019-11-28 08:56:58 +0100

    Update 0007.R

--- R/0007.R
+++ R/0007.R
@@ -1,11 +1,12 @@
 ### 0007
-is.prime <- function(n) n == 2L || all(n %% 2L:max(2,floor(sqrt(n))) != 0)
+is.prime <- function(n){ifelse(n!=1,(n == 2L & n!=1) || all(n %% 2L:max(2,floor(sqrt(n))) != 0),F)}
 
 prime_numbers <- c()
-for (n in (2:1000000)){
-  if (is.prime(n)){
-    prime_numbers <- c(prime_numbers, n)
-  }
+n <- 2
+count_ <- 1
+while(count_ < 10002){
+  if(is.prime(n)){count_ <- count_+1}
+  n <- n+1
 }
 
-prime_numbers[10001]
+n-1
