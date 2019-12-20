3fb6769a45339cedaf2a4fc3cbeda3ab73fef441
EGrandgi <egrandgi@gmail.com>
2019-12-20 17:19:49 +0100

    Create 0044.R

---
+++ R/0044.R
@@ -0,0 +1,16 @@
+### 0044
+X <- c(1:10000)
+pentagonals <- X*(3*X−1)/2
+
+for(i in 1:length(pentagonals)){
+  for(j in i:length(pentagonals)){
+    sum_ <- pentagonals[i] + pentagonals[j]
+    diff_ <- abs(pentagonals[i] - pentagonals[j])
+    if(sum_%in%pentagonals & diff_%in%pentagonals){
+      result <- diff_
+      break
+    }
+  } 
+}
+
+result
