bba590786141b8670b3ff8a3b638fd1445c0cad5
EGrandgi <egrandgi@gmail.com>
2019-12-20 23:25:32 +0100

    Update 0029.R

--- R/0029.R
+++ R/0029.R
@@ -1,8 +1,2 @@
 ### 0029
-# length(unique(sort(c(outer(2:100,2:100,"^")))))
-
-library("gmp")
-
-length(unique(do.call("c",sapply(as.bigz(2:100),function(a) a^(as.bigz(2:100))))))
-
-
+length(unique(sort(c(outer(2:100,2:100,"^")))))
